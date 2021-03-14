e75c4f8bb06095a2f1eca092029125adc95c9adb
amusich25 <77763959+amusich25@users.noreply.github.com>
2021-03-14 11:56:48 -0700

    formatted the conversion links

--- src/CurrentTemp.js
+++ src/CurrentTemp.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./currenttemp.css";
 
 export default function CurrentTemp(props) {
-  return (
+    const [unit, setUnit] = useState("fahrenheit");
+    function showCelsius(event) {
+        event.preventDefualt ();
+        setUnit("celsius");
+    }
+
+    function showFahrenheit(event) {
+        event.preventDefualt();
+        setUnit("fahrenheit");
+    }
+
+    function celsius() {
+        return (props.fahrenheit - 32) * 5/9;
+    }
+
+    if (unit==="fahrenhiet") {
+       return (
       <div className="float-left"> 
-      <span className="currenttemp">{props.data.temperature}</span>
+      <span className="currenttemp">{Math.round(props.fahrenheit)}</span>
       <span className="units">
         <a href="/" id="fahrenheit-link" className="active">
+          °F
+        </a>{" "}
+        |
+        <a href="/" id="celsius-link" onClick={showCelsius}>
+          °C
+        </a>
+      </span>
+      </div>
+  ); 
+    } else {
+        return (
+            <div className="float-left"> 
+      <span className="currenttemp">{Math.round(celsius())}</span>
+      <span className="units">
+        <a href="/" id="fahrenheit-link" className="active" onClick={showFahrenheit}>
           °F
         </a>{" "}
         |
@@ -15,5 +46,6 @@ export default function CurrentTemp(props) {
         </a>
       </span>
       </div>
-  );
+        );
+    }
 }
--- src/InfoBreakdown.js
+++ src/InfoBreakdown.js
@@ -5,7 +5,7 @@ export default function InfoBreakdown (props) {
     return (
        <div id ="info-breakdown">
                 <p>
-                Humidity: <span id = "humidity">{props.data.humity}</span>%<br />
+                Humidity: <span id = "humidity">{props.data.humidity}</span>%<br />
                 Wind Speed: <span id = "wind"></span> {props.data.wind} mph <br />
                 </p>
               </div>
